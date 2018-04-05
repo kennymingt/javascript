39d913a945233a96a1027f919015ad679185c602
Kent Tang Charlo <kennymingt@gmail.com>
2018-04-05 11:40:56 +0800

    Input text in js

---
+++ code/3-input-text-javascript/main.js
@@ -0,0 +1,17 @@
+/**
+ * This application shows how to read the value from an input field.
+ */
+
+console.log('Starting App')
+
+// Get a reference of the DOM elements
+const nameInput = document.getElementById('name')
+const button = document.getElementById('myButton')
+const messageElement = document.getElementById('message')
+
+// When the button is click it will execute a function.
+// This function gets the value of the name input and displays it in the message div element.
+button.addEventListener('click', function(){
+    const name =nameInput.value
+    messageElement.textContent = name
+})
